74637e72a38ab5b044ad3a6cb3b84007e96ffa96
sherryylin0719 <sherryylin0719@gmail.com>
2023-08-24 23:10:58 +0900

    feat: add mongoose encryption

--- models/seeds/userSeeder.js
+++ models/seeds/userSeeder.js
@@ -1,35 +1,35 @@
 const db = require('../../config/mongoose')
 const fetchData = require('./apiService')
-const bcrypt = require("bcryptjs")
 const User = require('../user')
 
 db.once('open', async () => {
   try {
     console.log('mongodb connected!')
     // create admin data
-    const rootSalt = await bcrypt.genSalt(10)
-    const rootHash = await bcrypt.hash('12345678', rootSalt)
     const rootUser = {
       name: 'root',
       email: 'root@example.com',
-      password: rootHash,
+      password: '12345678',
       role: 'admin',
     }
+
     // axios get data
     const userData = await fetchData('users?limit=5')
+    
     // create user data
     let newUsers = [rootUser]
     for (let i = 0; i < userData.length; i++) {
-      const salt = await bcrypt.genSalt(10)
-      const hash = await bcrypt.hash(userData[i].password, salt)
-      newUsers.push({
+      const newUser = {
         name: userData[i].username,
         email: userData[i].email,
-        password: hash
-      })
+        password: userData[i].password,
+      };
+      newUsers.push(newUser);
     }
+
     // insert data
-    await User.create(newUsers)
+    await User.create(newUsers);
+
     console.log('done')
     process.exit()
   } catch (error) {
--- models/user.js
+++ models/user.js
@@ -1,5 +1,11 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const mongoose = require('mongoose')
+const encryption = require('mongoose-encryption');
+const bcrypt = require("bcryptjs")
 const Schema = mongoose.Schema
+
 const userSchema = new Schema({
   name: {
     type: String,
@@ -38,4 +44,37 @@ const userSchema = new Schema({
     default: Date.now
   },
 })
+
+const encKey = process.env.ENC_SECRET;
+const sigKey = process.env.SIG_SECRET;
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+    return next()
+  } catch (err) {
+    return next(err)
+  }
+})
+
+// compare password
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  try {
+    if (!this.password) {
+      throw new Error('password not set')
+    } else {
+      const match = await bcrypt.compare(candidatePassword, this.password)
+      return match
+    }
+  } catch (err) {
+    console.error('Error comparing passwords:', err);
+    return false;
+  }
+}
+
+// Encrypt specified fields
+userSchema.plugin(encryption, { encryptionKey: encKey, signingKey: sigKey, encryptedFields: ['name','email','password','address','phone'] });
+
 module.exports = mongoose.model('User', userSchema)
