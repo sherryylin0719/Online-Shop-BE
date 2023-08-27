8456661819de6e7ad0c048c2c2e20c29c447f508
sherryylin0719 <sherryylin0719@gmail.com>
2023-08-27 18:33:37 +0900

    refactor: add orders into user seeder

--- models/seeds/userSeeder.js
+++ models/seeds/userSeeder.js
@@ -8,43 +8,43 @@ db.once('open', async () => {
   try {
     console.log('mongodb connected!')
     // create admin data
-    const rootSalt = await bcrypt.genSalt(10)
-    const rootHash = await bcrypt.hash('12345678', rootSalt)
-    const rootUser = {
-      name: 'root',
-      email: 'root@example.com',
-      password: rootHash,
-      role: 'admin',
-      phone: '0912345678',
-    }
+    // const rootSalt = await bcrypt.genSalt(10)
+    // const rootHash = await bcrypt.hash('12345678', rootSalt)
+    // const rootUser = {
+    //   name: 'root',
+    //   email: 'root@example.com',
+    //   password: rootHash,
+    //   role: 'admin',
+    //   phone: '0912345678',
+    // }
 
     // axios get data
-    const userData = await fetchData('users?limit=5')
+    // const userData = await fetchData('users?limit=5')
     
     // create user data
-    let newUsers = [rootUser]
-    for (let i = 0; i < userData.length; i++) {
-      const salt = await bcrypt.genSalt(10)
-      const hash = await bcrypt.hash(userData[i].password, salt)
-      const newUser = {
-        name: userData[i].username,
-        email: userData[i].email,
-        password: hash
-      };
-      newUsers.push(newUser);
-    }
+    // let newUsers = [rootUser]
+    // for (let i = 0; i < userData.length; i++) {
+    //   const salt = await bcrypt.genSalt(10)
+    //   const hash = await bcrypt.hash(userData[i].password, salt)
+    //   const newUser = {
+    //     name: userData[i].username,
+    //     email: userData[i].email,
+    //     password: hash
+    //   };
+    //   newUsers.push(newUser);
+    // }
 
     // insert data
-    await User.create(newUsers);
+    // await User.create(newUsers);
 
     // add order data into users
-    // const orderList = await Order.find().lean();
-    // for (let i = 0; i < orderList.length; i++) {
-    //   const orderId = orderList[i]._id;
-    //   const userId = orderList[i].userId;
+    const orderList = await Order.find().lean();
+    for (let i = 0; i < orderList.length; i++) {
+      const orderId = orderList[i]._id;
+      const userId = orderList[i].userId;
       
-    //   await User.findByIdAndUpdate(userId, { $addToSet: { orders: orderId } });
-    // }
+      await User.findByIdAndUpdate(userId, { $addToSet: { orders: orderId } });
+    }
 
     console.log('done')
     process.exit()
