48162325b768478256fd7c93f7312343862a9519
sherryylin0719 <sherryylin0719@gmail.com>
2023-08-31 09:51:53 +0900

    refactor: uploadImage function

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,18 +1,6 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+const uploadImage = require('../helpers/file-helper').uploadImage
 const productHelper = require('../helpers/product-helper');
 const Product = require('../models/product');
-const cloudinary = require('cloudinary').v2;
-const dataUri = require('../helpers/file-helper').dataUri
-
-// set up cloudinary
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
-  secure: true
-});
 
 const adminController = {
   getProducts: async (req, res, next) => {
@@ -26,30 +14,9 @@ const adminController = {
     try {
       const { title, price, category, description, quantity } = req.body
 
-      // upload image to cloudinary
-      const imageUrls = [];
-      for (let i = 0; i < req.files.length; i++) {
-        try {
-          const file = dataUri(req.files[i]).content;
-          const result = await cloudinary.uploader.upload(file, {
-          public_id: "products",
-          resource_type: "auto",
-          secure: true,
-          });
-          imageUrls.push(result.secure_url);
-        } catch (err) {
-          next(err)
-        }
-      }
-      // create image object
-      const image = []
-      for (let i = 0; i < imageUrls.length; i++) {
-        image.push({
-          url: imageUrls[i],
-          metadata: title
-        })
-      }
-
+      // upload image to cloudinary and return image object
+      const image = await uploadImage(req, res, next)
+      
       // create new product
       const product = await Product.create({
         title,
@@ -66,6 +33,14 @@ const adminController = {
           product: product
         }
       });
+    } catch (err) {
+      next(err)
+    }
+  },
+  editProduct: async (req, res, next) => {
+    try {
+      // upload image to cloudinary
+
     } catch (err) {
       next(err)
     }
--- helpers/file-helper.js
+++ helpers/file-helper.js
@@ -1,6 +1,9 @@
-const multer = require('multer')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 // set up multer
+const multer = require('multer')
 const storage = multer.memoryStorage();
 const multerUpload = multer({ storage }).array('images', 10);
 
@@ -9,8 +12,45 @@ const Datauri = require('datauri/parser')
 const dUri = new Datauri()
 const dataUri = req => dUri.format(req.originalname.toString(), req.buffer);
 
+// set up cloudinary
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+  secure: true
+});
+
+const uploadImage = async (req, res, next) => {
+  try {
+    // upload image to cloudinary
+    const imageUrls = [];
+    for (let i = 0; i < req.files.length; i++) {
+      const file = dataUri(req.files[i]).content;
+      const result = await cloudinary.uploader.upload(file, {
+      public_id: "products",
+      resource_type: "auto",
+      secure: true,
+      });
+      imageUrls.push(result.secure_url);
+    }
+    // create image object
+    const image = []
+    for (let i = 0; i < imageUrls.length; i++) {
+      image.push({
+        url: imageUrls[i],
+        metadata: title
+      })
+      return image
+    }
+  } catch (err) {
+    next(err)
+  }
+}
+
 module.exports = {
   multerUpload,
-  dataUri
+  dataUri,
+  uploadImage
 };
   
