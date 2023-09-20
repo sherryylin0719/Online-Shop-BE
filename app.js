9fcb30f28ea39dba24d3c57d74f0335989580735
sherryylin0719 <sherryylin0719@gmail.com>
2023-09-20 10:11:24 +0900

    chore: add cors

--- app.js
+++ app.js
@@ -5,11 +5,22 @@ const express = require('express')
 const session = require('express-session')
 const passport = require('./config/passport')
 const bodyParser = require("body-parser")
+const cors = require('cors')
 const apiRouter = require('./routes')
 require('./config/mongoose')
 
 const app = express()
 const PORT = process.env.PORT || 3000
+
+// cors
+const corsOptions = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+  allowedHeaders: 'Authorization,Content-Type'
+}
+app.use(cors(corsOptions))
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(passport.initialize())
